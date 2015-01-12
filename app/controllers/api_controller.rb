aec18ca7aa3bdf48fd42234db5a37f544d65181f
Grant Nelson <grantmnelsn@gmail.com>
2015-01-11 20:31:09 -0500

    api for courses works

---
+++ app/controllers/api_controller.rb
@@ -0,0 +1,10 @@
+class ApiController < ApplicationController
+	def courses
+		@courses = Course.all
+
+		respond_to do |format|
+			format.json { render :json => @courses }
+			format.html #index.html.erb
+		end
+	end
+end
