921bea2e73bc48cfa2333c8727f4011ad4d2e660
Grant Nelson <grantmnelsn@gmail.com>
2015-03-19 15:19:24 -0400

    serve static assets false

--- app/assets/javascripts/index.js
+++ app/assets/javascripts/index.js
@@ -51,7 +51,7 @@ $( document ).on('page:change', function() {
 
 	// if the user has blocked ads....
  	$.ajax( "http://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js" ).fail(function() {
- 		document.body.style.background = "url('tiedye.jpg') repeat-x";
+ 		document.body.style.background = "url('http://www.tie-dyes.com/images/Tapestry-Rainbow-Spiral-Tie-Dye-bg.jpg') repeat-x";
   })
 
 })
