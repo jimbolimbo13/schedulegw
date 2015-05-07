de91405021f6f6a659715ac757188ee441aa41d4
matthew gould <gouldm44@gmail.com>
2015-05-07 11:37:32 -0400

    added isbn scrape to course model

--- app/models/course.rb
+++ app/models/course.rb
@@ -19,4 +19,53 @@ class Course < ActiveRecord::Base
 		load Dir.pwd + '/GWU_scrape.rb'
 	end
 
+  def self.get_books(url)
+    yomu = Yomu.new 'url'
+    text = yomu.text
+    final_text = text.gsub(/\n+/, " ")
+    course_array = final_text.split(/\s(?=6\d{3}-\w{2,3})/)
+
+    course_array.each do |course|
+      gwid = course.match(/(6\d{3}-\w{2,3})/)
+      current_class = Course.find_by(gwid)
+      isbn_array = course.scan(/(?<=ISBN-13):*\s*((?:\d+-*)+)/)
+      if isbn_array != nil
+        isbn_array.map! {|x| x.to_s.slice(/((?:\d+-*)+)/).gsub("-", "")}
+        isbn_array.map! {|x| x[0] != "9" ? "978" + x : x}
+
+        isbn_array.each do |book|
+          current_class.isbn << book
+        end
+      end
+      isbn_array = course.scan(/(?<=ISBN[#:\s])\s*((?:\d+-?)+)/)
+      if isbn_array != nil
+        isbn_array.map! {|x| x.to_s.slice(/((?:\d+-*)+)/).gsub("-", "")}
+        isbn_array.map! {|x| x[0] != "9" ? "978" + x : x}
+
+        isbn_array.each do |book|
+          current_class.isbn << book
+        end
+      end
+      isbn_array = course.scan(/(?<=ISBN#:)\s*((?:\d+-?)+)/)
+      if isbn_array != nil
+        isbn_array.map! {|x| x.to_s.slice(/((?:\d+-*)+)/).gsub("-", "")}
+        isbn_array.map! {|x| x[0] != "9" ? "978" + x : x}
+
+        isbn_array.each do |book|
+          current_class.isbn << book
+        end
+      end
+      isbn_array = course.scan(/(?<=ISBN\sNumber:)\s*((?:\d+-?)+)/)
+      if isbn_array != nil
+        isbn_array.map! {|x| x.to_s.slice(/((?:\d+-*)+)/).gsub("-", "")}
+        isbn_array.map! {|x| x[0] != "9" ? "978" + x : x}
+
+        isbn_array.each do |book|
+          current_class.isbn << book
+        end
+      end
+    end
+
+  end
+
 end
