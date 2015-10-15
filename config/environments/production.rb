04edf3c522bca9a5230226004eea1bb5a01e440f
Grant Nelson <grantmnelsn@gmail.com>
2015-10-15 16:20:53 -0400

    precompiled assets for deploy

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -19,7 +19,7 @@ Rails.application.configure do
   # For large-scale production use, consider using a caching reverse proxy like nginx, varnish or squid.
   # config.action_dispatch.rack_cache = true
 
-  # Disable Rails's static asset server (Apache or nginx will already do this).
+  # This should be set to false on production so that Heroku serves the assets instead of the app's pipeline
   config.serve_static_files = false
 
   # Compress JavaScripts and CSS.
